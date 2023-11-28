91ef74d1ee6f5afa4772e98f946f87f2f4994242
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-28 23:52:50 +0200

    Add files via upload

---
+++ 02. Vowels Count/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            VowelsCount(input);
+        }
+
+        private static void VowelsCount(string text)
+        {
+            int count = 0;
+
+            foreach (var character in text)
+            {
+                if (IsVowel(character))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+
+        private static bool IsVowel(char character)
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y', };
+            return vowels.Contains(character);
+        }
+    }
+}
