5943309b04a8c2d57068cfa5ccb1cc8e32b96301
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-28 23:51:53 +0200

    Add files via upload

---
+++ 01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01.Smallest_Of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            SmallestNumber(firstNumber, secondNumber, thirdNumber);
+        }
+
+        private static void SmallestNumber(int a, int b, int c)
+        {
+            if (a <= b && a <= c)
+            {
+                Console.WriteLine(a);
+            }
+            else if (b <= a && b <= c)
+            {
+                Console.WriteLine(b);
+            }
+            else if (c <= a && c <= b)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
