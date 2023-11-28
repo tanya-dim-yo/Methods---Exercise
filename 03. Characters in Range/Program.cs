4686dc262cd703474d60e6ef91b6fb583d998979
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-28 23:53:25 +0200

    Add files via upload

---
+++ 03. Characters in Range/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _03.Characters_In_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+
+            if (firstChar > secondChar)
+            {
+                char temp = firstChar;
+                firstChar = secondChar;
+                secondChar = temp;
+            }
+
+            PrintCharactersRange(firstChar, secondChar);
+
+        }
+
+        private static void PrintCharactersRange(char a, char b)
+        {
+            for (int i = a + 1; i < b; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+        }
+    }
+}
