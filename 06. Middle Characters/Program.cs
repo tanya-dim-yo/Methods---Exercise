2ea7f05445b7b0678e2ad0d54887c7d2922a3829
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-30 03:51:49 +0200

    Add files via upload

---
+++ 06. Middle Characters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _06.Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            PrintMiddleCharacter(input);
+        }
+
+        private static void PrintMiddleCharacter(string input)
+        {
+            if (input.Length % 2 == 0)
+            {
+                Console.WriteLine($"{input[(input.Length / 2) - 1]}{input[input.Length / 2]}");
+            }
+            else
+            {
+                Console.WriteLine($"{input[input.Length / 2]}");
+            }
+        }
+    }
+}
