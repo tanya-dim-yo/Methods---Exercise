c799313d68392bbd8eeef84502ae276414aefc4a
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-30 03:52:29 +0200

    Add files via upload

---
+++ 07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _07.NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintMatrix(n);
+        }
+
+        private static void PrintMatrix(int n)
+        {
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 1; cols <= n; cols++)
+                {
+                    Console.Write(n + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
