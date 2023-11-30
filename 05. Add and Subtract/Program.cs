6cbc764b3555c6bfad22f1dc94a51889ab368c09
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-11-30 03:51:14 +0200

    Add files via upload

---
+++ 05. Add and Subtract/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _05.Add_And_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int result = SubtractCalculation(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(result);
+        }
+
+        private static int SubtractCalculation(int a, int b, int c)
+        {
+            return AddCalculation(a, b) - c;
+        }
+
+        private static int AddCalculation(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
