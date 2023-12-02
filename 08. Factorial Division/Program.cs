4af640316ce9371d333e55fcaba90deb8653e86b
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-02 04:43:48 +0200

    Add files via upload

---
+++ 08. Factorial Division/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _08.Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0:F2}", (double)Factorial(firstNumber) / Factorial(secondNumber));
+        }
+
+        private static long Factorial(int a)
+        {
+            long factorial = 1;
+
+            for (int i = 1; i <= a; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
