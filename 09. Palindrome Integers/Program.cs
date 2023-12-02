542ad7ebc53f7bd9428aff7db6a04b7e05f8594e
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-02 04:44:31 +0200

    Add files via upload

---
+++ 09. Palindrome Integers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _09.Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (IsPalindrome(input))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+
+        private static bool IsPalindrome(string input)
+        {
+            bool result = true;
+
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+    }
+}
